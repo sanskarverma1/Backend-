597ccc381b91bab6a4b94090fcfde03085ed6270
sanskarverma1 <sanskar.karens@gmail.com>
2024-01-25 20:23:22 +0530

    Ts errors

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -1,22 +1,26 @@
 import { Request, Response, NextFunction } from "express"
 import { ErrorCode, HttpException } from "./exceptions/root"
 import { InternalException } from "./exceptions/internal-exception"
+import { ZodError } from "zod"
+import { BadRequestsException } from "./exceptions/bad-request"
 
-export const errorHandler=(method : Function)=>{
-return async (req:Request, res:Response, next:NextFunction)=>{
-    try{
-        await method(req,res,next)
-    }
-    catch(error: any){
-        let exception: HttpException;
-        if(error instanceof HttpException){
-                exception=error;
+export const errorHandler = (method: Function) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            await method(req, res, next)
+        } catch(error: any) {
+            let exception: HttpException;
+            if( error instanceof HttpException) {
+                exception = error;
+            } else {
+                if( error instanceof ZodError) {
+                    exception = new BadRequestsException('Unprocessable entity.', ErrorCode.UNPROCESSABLE_ENTITY)
+                } else {
+                    exception = new InternalException('Something went wrong!', error, ErrorCode.INTERNAL_EXCEPTION)
+                }
+            }
+            next(exception)
         }
-        else{
-            exception = new InternalException('Something Went Wrong', error, ErrorCode.INTERNAL_EXCEPTION)
-        }
-        next(exception)
-    }
 
-}
+    }
 }
--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -5,35 +5,30 @@ import * as jwt from 'jsonwebtoken'
 import { JWT_SECRET } from "../secrets";
 import { prismaClient } from "..";
 
-const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-    // 1. Extract the token from header
-    const token = req.headers.authorization;
-
-    // 2. if token is not present throw an error of unauthorized
-    if (!token) {
-        next(new UnauthorizedException('Unauthorized', ErrorCode.UNAUTHORIZED));
-        return; // Ensure we exit the function after calling `next`
-    }
-
-    // 3. if token is present, verify the token and extract the payload
-    try {
-        const payload = jwt.verify(token as string, JWT_SECRET) as any;
-
-        // 4. get the user from the payload
-        const user = await prismaClient.user.findFirst({ where: { id: payload.userId } });
-
-        // 5. if user is null, throw an error of unauthorized
-        if (!user) {
-            next(new UnauthorizedException('Unauthorized', ErrorCode.UNAUTHORIZED));
-           // return; // Ensure we exit the function after calling `next`
-        }
-
-        // 6. to attach the user to the current req object
-        req.user = user;
-        next(); // Continue to the next middleware or route handler
-    } catch (error) {
-        next(new UnauthorizedException('Unauthorized', ErrorCode.UNAUTHORIZED));
-    }
+const authMiddleware = async(req: Request, res:Response, next:NextFunction) => {
+// 1. extract the token from header
+const token = req.headers.authorization!
+// 2. if token is not present, throw an error of unauthorized
+if(!token) {
+    next(new UnauthorizedException('Unauthorized', ErrorCode.UNAUTHORIZED))
+}
+try {
+// 3. if the token is present, verify that token and extract the payload
+const payload = jwt.verify(token, JWT_SECRET) as any
+// 4. to get the user from the payload
+const user = await prismaClient.user.findFirst({where: {id: payload.userId}})
+if (!user) {
+    next(new UnauthorizedException('Unauthorized', ErrorCode.UNAUTHORIZED))
+}
+// 5. to attach the user to the current request obejct
+req.user = user!
+next()
+}
+catch(error) {
+    next(new UnauthorizedException('Unauthorized', ErrorCode.UNAUTHORIZED))
 }
 
-export default authMiddleware;
+
+}
+
+export default authMiddleware
